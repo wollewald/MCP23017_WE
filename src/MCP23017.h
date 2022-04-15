cdfdf7a041975bf06cfeec147c1201399f9cffe3
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-04-15 12:08:17 +0200

    Update MCP23017.h

--- src/MCP23017.h
+++ src/MCP23017.h
@@ -1,5 +1,5 @@
 /*****************************************
-This is a library for the MCP23017 and MCP23S17 I/O Port Expander
+This is a library for the MCP23017/MCP23S17/MCP23018/MCP23S18 I/O Port Expander.
 
 You'll find some examples including the wiring which should enable you to use the library. 
 Furthermore I have added a list of commands.
