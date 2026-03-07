49e6d3cd4a12cb216118abf78ba2d6e762cbf762
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2026-03-07 14:19:40 +0100

    Add files via upload

--- examples/mcp23017_gpio_reading/mcp23017_gpio_reading.ino
+++ examples/mcp23017_gpio_reading/mcp23017_gpio_reading.ino
@@ -12,7 +12,7 @@ https://wolles-elektronikkiste.de/en/port-expander-mcp23017-2
 
 *******************************************************/
 #include <Wire.h>
-#include <MCP23017.h>
+#include <MCP23017_WE.h>
 #define MCP_ADDRESS 0x20 // (A2/A1/A0 = LOW)
 
 /* A hardware reset is performed during init(). If you want to save a pin you can define a dummy 
@@ -20,13 +20,17 @@ https://wolles-elektronikkiste.de/en/port-expander-mcp23017-2
  * hardware reset. 
  */
 #define RESET_PIN 5
+
+typedef MCP23017_WE MCP; // just for less typing!
+
 /* There are several ways to create your MCP23017 object:
- * MCP23017 myMCP = MCP23017(MCP_ADDRESS) -> uses Wire / no reset pin 
- * MCP23017 myMCP = MCP23017(MCP_ADDRESS, RESET_PIN) -> uses Wire / RESET_PIN
- * MCP23017 myMCP = MCP23017(&Wire, MCP_ADDRESS) -> passing a TwoWire object / no reset pin
- * MCP23017 myMCP = MCP23017(&Wire, MCP_ADDRESS, RESET_PIN) -> "all together"
+ * MCP23017_WE myMCP = MCP23017_WE(MCP_ADDRESS) -> uses Wire / no reset pin 
+ * MCP23017_WE myMCP = MCP23017_WE(MCP_ADDRESS, RESET_PIN) -> uses Wire / RESET_PIN
+ * MCP23017_WE myMCP = MCP23017_WE(&Wire, MCP_ADDRESS) -> passing a TwoWire object / no reset pin
+ * MCP23017_WE myMCP = MCP23017_WE(&Wire, MCP_ADDRESS, RESET_PIN) -> "all together"
  */
-MCP23017 myMCP = MCP23017(MCP_ADDRESS, RESET_PIN);
+MCP myMCP = MCP(MCP_ADDRESS, RESET_PIN); // short version
+// MCP23017_WE myMCP = MCP23017_WE(MCP_ADDRESS, RESET_PIN); // long version
 
 int wT = 1000; // wT = waiting time
 byte portStatus;
@@ -39,17 +43,17 @@ void setup(){
     Serial.println("Not connected!");
     while(1){} 
   }  
-  myMCP.setPortMode(0b00000000, A);  // Port A: all pins are INPUT
-  myMCP.setPortPullUp(0b11110000, A);  // Port A: Pin 4 - 7 are pulled up
+  myMCP.setPortMode(0b00000000, MCP::A);  // Port A: all pins are INPUT
+  myMCP.setPortPullUp(0b11110000, MCP::A);  // Port A: Pin 4 - 7 are pulled up
 }
 
 void loop(){ 
   
-  portStatus = myMCP.getPort(A); // query the complete port status 
+  portStatus = myMCP.getPort(MCP::A); // query the complete port status 
   Serial.print("Status GPIO A: ");
   Serial.println(portStatus, BIN);
   
-  pinStatus = myMCP.getPin(5, A); // query one pin status
+  pinStatus = myMCP.getPin(5, MCP::A); // query one pin status
   Serial.print("Status Port A, Pin 5: ");
   Serial.println(pinStatus, BIN);
  
